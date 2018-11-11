00b3c6fea5e3fb2a8e6cca3ab566dbdb3b60023c
Dan Hintz <dhintz@live.com>
2018-11-11 22:57:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,12 +59,9 @@ class Student
     student = Student.new(row[0], row[1], row[2])
   end
   
-  def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT * FROM students WHERE name = ?
-    SQL
-    row = DB[:conn].execute(sql, name)
-    Student.new_from_db(row)
-  end
+ def self.find_by_name(name)
+   row = DB[:conn].execute("SELECT * FROM students WHERE name = ?", name)
+   Student.new_from_db(row)
+ end
 
 end
