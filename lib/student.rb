de59560b6e0177114ac490ad67b9211e42cdffc1
Dan Hintz <dhintz@live.com>
2018-11-11 22:12:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   end
   
   def update
-    sql <<-SQL
+    sql = <<-SQL
     UPDATE students SET name = ?, grade = ? WHERE id = ?
     SQL
     
@@ -55,7 +55,7 @@ class Student
   end
   
   def self.find_by_name(name)
-    sql <<-SQL
+    sql = <<-SQL
     SELECT * FROM students WHERE name=?
     SQL
     
