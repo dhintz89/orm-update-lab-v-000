c911060e2d8372da13511bacdb1321eda96bbdc9
Dan Hintz <dhintz@live.com>
2018-11-11 22:01:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
   
   def update(student)
     sql <<-SQL
-    UPDATE students SET 
+    UPDATE students SET name, 
   
   def self.new_from_db(row)
     Student.new(id: row[0], name: row[1], grade: row[3])
